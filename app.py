a7609693e4dd5040403fe9bb00128f21c04753aa
lourencoborges <lourencoborgessilva@gmail.com>
2026-04-05 13:44:03 -0300

    atualizacao

--- app.py
+++ app.py
@@ -23,31 +23,33 @@ def exibir_opcoes():
       print('4. sair\n')
 
 def finalizar_app():
-    os.system('cls')
-    print('finalizando o app\n')
+    exibir_subtitulo('Finaliar app')
 
+def voltar_ao_menu_principal():
+     input('\nDigite uma tecla para voltar ao menu principal ')
+     main()
 
 def opcao_invalida():
     print('opcao invalida!\n')
-    input('digite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+      os.system('cls')
+      print(texto)
+      print()
 
 def cadastrar_novo_restaurante():
-     os.system('cls')
-     print('cadastro de novos restaurantes\n')
+     exibir_subtitulo('Cadastro de novos restaurantes')
      nome_do_restaurante = input('digite o nome do restaurante que deseja cadastrar ')
      restaurantes.append(nome_do_restaurante)
      print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso\n')
-     input('\nDigite uma tecla para voltar ao menu principal')
-     main()
+     voltar_ao_menu_principal()
 
 def listar_restaurantes():
-     os.system('cls')
-     print('Listando os restaurantes\n')
+     exibir_subtitulo('Listar restaurantes')
      for restaurante in restaurantes:
          print(f'.{restaurante}')
-     input('\nDigite uma tecla para voltar ao menu principal')
-     main()
+     voltar_ao_menu_principal()
 
 def escolher_opcao():
       try:
